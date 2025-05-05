09ddb0faa44a1ec413b3e733dcf4ff19c71257b8
mavila01-prog <mavila@crncy-group.com>
2025-05-04 21:24:34 -0600

    more error fix

--- apps/order-microservice/src/app/app.controller.ts
+++ apps/order-microservice/src/app/app.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get, Inject, Logger } from '@nestjs/common';
-import { ClientKafka, EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
-import { CreateOrderDto, KafkaTopics, OrderResponseDto } from '@kafka-tutorial/shared';
+import {
+  CreateOrderDto,
+  KafkaTopics,
+  OrderResponseDto,
+} from '@kafka-tutorial/shared';
 
 @Controller()
 export class AppController {
   private readonly logger = new Logger(AppController.name);
-  
+
   constructor(
     private readonly appService: AppService,
-    @Inject('KAFKA_SERVICE') private readonly kafkaService: ClientKafka,
+    @Inject('KAFKA_SERVICE') private readonly kafkaService: ClientKafka
   ) {}
 
   @Get()
@@ -20,14 +24,14 @@ export class AppController {
   @MessagePattern(KafkaTopics.ORDER_CREATED)
   processOrder(@Payload() orderData: CreateOrderDto): OrderResponseDto {
     this.logger.log(`Order received: ${JSON.stringify(orderData)}`);
-    
+
     // Process the order
     const result = this.appService.processOrder(orderData);
-    
+
     // After processing, notify interested services about order completion
     // This is optional and can be done alongside returning the response
-    console.log("[Order Received]: ", orderData);
-    
+    console.log('[Order Received]: ', orderData);
+
     this.logger.log(`Order processed: ${orderData.productId}`);
 
     //simulate processing the order
@@ -39,9 +43,9 @@ export class AppController {
         quantity: result.data.quantity,
       },
       status: result.status,
-      timestamp: result.timestamp
+      timestamp: result.timestamp,
     });
-    
+
     // With MessagePattern, this result is sent back to the requesting service
     return result;
   }
@@ -49,7 +53,7 @@ export class AppController {
   // @EventPattern(KafkaTopics.ORDER_CREATED)
   // async processOrderEvent(@Payload() orderData: CreateOrderDto): Promise<OrderResponseDto> {
   //   this.logger.log(`Order received: ${JSON.stringify(orderData)}`);
-    
+
   //   // Process the order
   //   const result = this.appService.processOrder(orderData);
   //   return result;
